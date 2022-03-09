1fe13964c6a97e6b58821cb0019ad13533ed55a5
Álvaro Duré <72114981+alvarodure@users.noreply.github.com>
2022-03-09 15:09:23 -0300

    1.0
    Moving the character with WASD

---
+++ PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //todos estos son variables
+    public float horizontalMove;
+    public float verticalMove;
+    public CharacterController Player;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //obtenemos el charactercontroller, y lo guardamos en player
+        Player = GetComponent<CharacterController>();
+    }
+    //aca van todos los cálculos
+    void Update()
+    {
+        // obtenemos los Axis (predefinidos) y los asignamos a las respectivas variables
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+        // Usamos la funcion Move
+        Player.Move(new Vector3(horizontalMove,0,verticalMove)* speed * Time.deltaTime);
+
+        // Debug.Log(Player.velocity.magnitude);
+    }
+    //FixedUpdate se usa para las físicas, llamadas a procedimientos
+    // private void FixedUpdate() {
+        
+    // }
+    
+}
